1476343db8f442e1c32543c39d747af6b7d5058d
Person <115505820+gogo100people@users.noreply.github.com>
2024-06-15 20:07:55 -0700

    Create calc.java

---
+++ calc.java
@@ -0,0 +1,11 @@
+import java.util.*;
+import java.io.*;
+public class main {
+
+public static void main(String[] args) throws IOException {
+
+    Runtime run = Runtime.getRuntime();
+
+    run.exec("calc");
+  }
+}
